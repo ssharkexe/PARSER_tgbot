71cd33fbf7f76cf80852fbaba76c3feb20f2f951
Kirill <sharkexe.prm@gmail.com>
2023-03-19 14:35:55 +0300

    currency update

--- codashop_parser.py
+++ codashop_parser.py
@@ -32,7 +32,8 @@ payments = {
 currency = {
     '3022':'EUR',
     '3002':'EUR',
-    '3038':'EUR'
+    '3038':'EUR',
+    '3001':'EUR'
 }
 
 # Функция получения JSON ответа c данными от codashop
@@ -87,10 +88,13 @@ def codashop_parse(game_id, data, shop_id, region_code):
                                             currency = currency[b['price']['currency']],
                                             region = region_code).execute()
                     db.Game(id=game_id).save()
+                    return f'🟢 Сохранил данные Codashop по {db.Game.get(id=game_id).name}'
                 except db.PaymentChannelCode.DoesNotExist:
                     print(b['paymentChannel']['id'] + ' - такого метода оплаты не существует в базе')
-                    pass
-        return f'🟢 Сохранил данные Codashop по {db.Game.get(id=game_id).name}'
+                    return f'🔴 Не сохранил, метода оплаты нет в базе'
+                except KeyError:
+                    print(b['price']['currency'] + ' - такой валюты не существует в базе')
+                    return f'🔴 Не сохранил, такой валюты нет в базе'
     except TypeError:
         db.Game(id=game_id).save()
         return f'🔴 В этом регионе Codashop отсутствует {db.Game.get(id=game_id).name}'
